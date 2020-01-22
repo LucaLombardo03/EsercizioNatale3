ec8f87c03c4618332d3e6bb62005d719b4deb8f5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-22 12:50:43 +0100

    fine app

--- EsercizioNatale3/MainWindow.xaml.cs
+++ EsercizioNatale3/MainWindow.xaml.cs
@@ -20,27 +20,60 @@ namespace EsercizioNatale3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TextBox[] array;
         public MainWindow()
         {
             InitializeComponent();
+            array = new TextBox[] { TxtNum1, TxtNum2, TxtNum3, TxtNum4, TxtNum5, TxtNum6 };
         }
+        
         private void BtnON_OFF_Click(object sender, RoutedEventArgs e)
         {
-            if (BtnCalcola.Content.ToString() == "ON")
+            if (BtnON_OFF.Content.ToString() == "ON")
             {
-                TxtNum1.IsEnabled = false;
-                TxtNum2.IsEnabled = false;
-                TxtNum3.IsEnabled = false;
-                TxtNum4.IsEnabled = false;
-                TxtNum5.IsEnabled = false;
-                TxtNum6.IsEnabled = false;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].IsEnabled = true;
+                }
+                BtnCalcola.IsEnabled = true;
                 BtnON_OFF.Content = "OFF";
-            } 
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i].IsEnabled = false;
+                }
+                BtnCalcola.IsEnabled = false;
+                BtnON_OFF.Content = "ON";
+            }
+
         }
 
         private void BtnCalcola_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                int n1 = 0;
+                int n2 = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int num = int.Parse(array[i].Text);
+                    if (num % 2 == 0)
+                    {
+                        n1++;
+                    }
+                    else
+                    {
+                        n2++;
+                    }
+                }
+                LblRisultato.Content = $"I numeri pari sono {n1}, i numeri dispari sono {n2}";
+            }
+            catch
+            {
+                MessageBox.Show("Inserire solo numeri", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
         }
     }
 }
